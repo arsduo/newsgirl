faa193cf7d0a03ec1d7818637ee2c882f5e5430c
Alex Koppel <alex+git@alexkoppel.com>
2012-11-04 13:57:20 +0100

    Fix spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,4 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
-require 'rspec/rails'
-require 'faker'
-require 'timecop'
-
-Rails.backtrace_cleaner.remove_silencers!
+require "changelogger"
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
-
-RSpec.configure do |config|
-  config.before :each do
-    BatchApi.config.limit = 20
-    BatchApi.config.endpoint = "/batch"
-    BatchApi.config.verb = :post
-
-    BatchApi.stub(:rails?).and_return(false)
-  end
-end
